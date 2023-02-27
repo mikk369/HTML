b2abd6b17783755c81c55e73fb9b7fc82d3e8dd4
mikk369 <mikk.ereline@vikk.ee>
2023-02-27 17:54:32 +0200

    accordion open/close

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ for (const item of items) {
     item.classList.toggle("open");
   });
 }
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// let person1 = new Person("John", 30);
+// let person2 = new Person("Jane", 25);
+// console.log(person1);
