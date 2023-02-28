3ddd4c3ff1b4aa9a9b7059e0e245197f93c381e4
mikk369 <mikk.ereline@vikk.ee>
2023-02-28 15:47:25 +0200

    next and prev button working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ACCORDION
 const items = document.querySelectorAll(".item");
 
 for (const item of items) {
@@ -6,11 +7,38 @@ for (const item of items) {
   });
 }
 
-// function Person(name, age) {
-//   this.name = name;
-//   this.age = age;
-// }
+// CAROUSELL
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const carousel = document.querySelector(".carousel");
+const dots = document.querySelectorAll(".dot");
+testimonials = document.querySelectorAll(".testimonial");
 
-// let person1 = new Person("John", 30);
-// let person2 = new Person("Jane", 25);
-// console.log(person1);
+let currentSlide = 0;
+// Show the current slide and active dot
+function showSlide(slideIndex) {
+  // Hide all slides and remove active class from all dots
+  testimonials.forEach((testimonial) => testimonial.classList.remove("active"));
+  dots.forEach((dot) => dot.classList.remove("dot-active"));
+  // Show the current slide and activate the corresponding dot
+  testimonials[slideIndex].classList.add("active");
+  dots[slideIndex].classList.add("dot-active");
+}
+// Show the first slide and active dot on page load
+showSlide(currentSlide);
+// Add event listeners to carousel buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id === "left") {
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = testimonials.length - 1;
+      }
+    } else if (button.id === "right") {
+      currentSlide++;
+      if (currentSlide >= testimonials.length) {
+        currentSlide = 0;
+      }
+    }
+    showSlide(currentSlide);
+  });
+});
